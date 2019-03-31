3a7a2d0676a117b77cfc1370d8d469162feb8fe6
Anastasia Kandratsina <kandratsina@gmail.com>
2019-03-31 22:53:45 +0300

    Add employees;

---
+++ ProjectGraduationK/Controllers/EmployeesController.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProjectGraduationK.Data;
+using ProjectGraduationK.Models;
+
+namespace ProjectGraduationK.Controllers
+{
+    [Route("employees")]
+    [Authorize]
+    public class EmployeesController : Controller
+    {
+
+        private GraduationDbContext _context;
+        private UserManager<User> _userManager;
+
+        public EmployeesController(GraduationDbContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Employees()
+        {
+            var users = _userManager.Users.ToList();
+            return View(users);
+        }
+    }
+}
