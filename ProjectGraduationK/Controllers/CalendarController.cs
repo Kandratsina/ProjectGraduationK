869f7cb03b331a0736d9db4d608ade1d263a7b52
Anastasia Kandratsina <kandratsina@gmail.com>
2019-03-31 22:52:59 +0300

    Add calendar;

---
+++ ProjectGraduationK/Controllers/CalendarController.cs
@@ -0,0 +1,79 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProjectGraduationK.Data;
+using ProjectGraduationK.Models;
+
+namespace ProjectGraduationK.Controllers
+{
+    [Route("calendar")]
+    [Authorize]
+    public class CalendarController : Controller
+    {
+        private GraduationDbContext _context;
+
+        public CalendarController(GraduationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Calendar()
+        {
+            return View();
+        }
+
+        [HttpGet("GetEvents")]
+        public JsonResult GetEvents()
+        {
+            var events = _context.CalendarEvents.ToList();
+            return new JsonResult(events);
+        }
+
+
+        [HttpPost("SaveEvent")]
+        public JsonResult SaveEvent(CalendarEvent calendarEvent)
+        {
+            var status = false;
+
+            if (calendarEvent.EventId > 0)
+            {
+                //Update the event
+                var eventToUpdate = _context.CalendarEvents.FirstOrDefault(e => e.EventId == calendarEvent.EventId);
+                if (eventToUpdate != null)
+                {
+                    eventToUpdate.Subject = calendarEvent.Subject;
+                    eventToUpdate.Start = calendarEvent.Start;
+                    eventToUpdate.End = calendarEvent.End;
+                    eventToUpdate.Description = calendarEvent.Description;
+                    eventToUpdate.IsFullDay = calendarEvent.IsFullDay;
+                    eventToUpdate.ThemeColor = calendarEvent.ThemeColor;
+                }
+            }
+            else
+            {
+                _context.CalendarEvents.Add(calendarEvent);
+            }
+            _context.SaveChanges();
+            status = true;
+
+            return new JsonResult(status);
+        }
+
+        [HttpDelete("{eventId}")]
+        public JsonResult DeleteEvent(int eventId)
+        {
+            var status = false;
+
+            var eventToUpdate = _context.CalendarEvents.Where(a => a.EventId == eventId).FirstOrDefault();
+            if (eventToUpdate != null)
+            {
+                _context.CalendarEvents.Remove(eventToUpdate);
+                _context.SaveChanges();
+                status = true;
+            }
+
+            return new JsonResult(status);
+        }
+    }
+}
---
+++ ProjectGraduationK/Models/CalendarEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectGraduationK.Models
+{
+    public class CalendarEvent
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EventId { get; set; }
+        public string Subject { get; set; }
+        public string Description { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string ThemeColor { get; set; }
+        public bool IsFullDay { get; set; }
+    }
+}
