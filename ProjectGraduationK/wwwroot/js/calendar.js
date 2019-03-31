68f1d0cfa1dbf359f8c4ab2af4d69ed7f9fec8b7
Anastasia Kandratsina <kandratsina@gmail.com>
2019-03-31 22:50:18 +0300

    Initial commit;

---
+++ ProjectGraduationK/wwwroot/js/calendar.js
@@ -0,0 +1,200 @@
+﻿$(document).ready(function () {
+    let events = [];
+    let selectedEvent = null;
+    FetchEventAndRenderCalendar();
+
+    function FetchEventAndRenderCalendar() {
+        events = [];
+        $.ajax({
+            type: "GET",
+            url: "/calendar/GetEvents",
+            success: function (data) {
+                $.each(data, function (k, v) {
+                    events.push({
+                        eventId: v.eventId,
+                        title: v.subject,
+                        description: v.description,
+                        start: moment(v.start),
+                        end: v.end != null ? moment(v.end) : null,
+                        color: v.themeColor,
+                        allDay: v.isFullDay
+                    });
+                })
+
+                GenerateCalendar(events);
+            },
+            error: function (error) {
+                alert('failed');
+            }
+        })
+    }
+
+    function GenerateCalendar(events) {
+        $(' ').fullCalendar('destroy');
+        $('#calendar').fullCalendar({
+            contentHeight: "auto",
+            firstDay: 1,
+            defaultDate: new Date(),
+            timeFormat: 'h(:mm)a',
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'month,basicWeek,basicDay'
+            },
+            monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+            monthNamesShort: ['Янв.', 'Фев.', 'Март', 'Апр.', 'Май', 'Июнь', 'Июль', 'Авг.', 'Сент.', 'Окт.', 'Ноя.', 'Дек.'],
+            dayNames: ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"],
+            dayNamesShort: ["ВС", "ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ"],
+            buttonText: {
+                today: "Сегодня",
+                month: "Месяц",
+                week: "Неделя",
+                day: "День",
+            }, 
+            eventLimit: true,
+            eventColor: '#378006',
+            events: events,
+            eventClick: function (calEvent) {
+                selectedEvent = calEvent;
+                $('#myModal #eventTitle').text(calEvent.title);
+                let $description = $('<div/>');
+                $description.append($('<p/>').html('<b>Начало: </b>' + calEvent.start.format("DD-MMM-YYYY HH:mm a")));
+                if (calEvent.end != null) {
+                    $description.append($('<p/>').html('<b>Конец: </b>' + calEvent.end.format("DD-MMM-YYYY HH:mm a")));
+                }
+                $description.append($('<p/>').html('<b>Описание: </b>' + calEvent.description));
+                $('#myModal #pDetails').empty().html($description);
+                $('#myModal').modal();
+            },
+            selectable: true,
+            select: function (start, end) {
+                selectedEvent = {
+                    eventID: 0,
+                    title: '',
+                    description: '',
+                    start: start,
+                    end: end,
+                    allDay: false,
+                    color: ''
+                };
+                openAddEditForm();
+                $('#calendar').fullCalendar('unselect');
+            },
+            editable: true,
+            eventDrop: function (event) {
+                let data = {
+                    EventID: event.eventID,
+                    Subject: event.title,
+                    Start: event.start.format('DD/MM/YYYY HH:mm A'),
+                    End: event.end != null ? event.end.format('DD/MM/YYYY HH:mm A') : null,
+                    Description: event.description,
+                    ThemeColor: event.color,
+                    IsFullDay: event.allDay
+                };
+                SaveEvent(data);
+            }
+        })
+    }
+
+    $('#btnEdit').click(function () {
+        //Open modal dialog for edit event
+        openAddEditForm();
+    })
+    $('#btnDelete').click(function () {
+        if (selectedEvent != null && confirm('Are you sure?')) {
+            $.ajax({
+                type: "DELETE",
+                url: '/calendar/'.concat(selectedEvent.eventId),
+                success: function (data) {
+                    if (data.status) {
+                        //Refresh the calendar
+                        FetchEventAndRenderCalendar();
+                        $('#myModal').modal('hide');
+                    }
+                },
+                error: function () {
+                    alert('Failed');
+                }
+            })
+        }
+    })
+    $('#dtp1,#dtp2').datetimepicker({
+        format: 'DD/MM/YYYY HH:mm A'
+    });
+    $('#chkIsFullDay').change(function () {
+        if ($(this).is(':checked')) {
+            $('#divEndDate').hide();
+        }
+        else {
+            $('#divEndDate').show();
+        }
+    });
+
+    function openAddEditForm() {
+        if (selectedEvent != null) {
+            $('#hdEventID').val(selectedEvent.eventId);
+            $('#txtSubject').val(selectedEvent.title);
+            $('#txtStart').val(selectedEvent.start.format('DD/MM/YYYY HH:mm A'));
+            $('#chkIsFullDay').prop("checked", selectedEvent.allDay || false);
+            $('#chkIsFullDay').change();
+            $('#txtEnd').val(selectedEvent.end != null ? selectedEvent.end.format('DD/MM/YYYY HH:mm A') : '');
+            $('#txtDescription').val(selectedEvent.description);
+            $('#ddThemeColor').val(selectedEvent.color);
+        }
+        $('#myModal').modal('hide');
+        $('#myModalSave').modal();
+        
+    }
+    $('#btnSave').click(function () {
+        //Validation/
+        if ($('#txtSubject').val().trim() == "") {
+            alert('Subject required');
+            return;
+        }
+        if ($('#txtStart').val().trim() == "") {
+            alert('Start date required');
+            return;
+        }
+        if ($('#chkIsFullDay').is(':checked') == false && $('#txtEnd').val().trim() == "") {
+            alert('End date required');
+            return;
+        }
+        else {
+            let startDate = moment($('#txtStart').val(), "DD/MM/YYYY HH:mm A").toDate();
+            let endDate = moment($('#txtEnd').val(), "DD/MM/YYYY HH:mm A").toDate();
+            if (startDate > endDate) {
+                alert('Invalid end date');
+                return;
+            }
+        }
+
+        let data = {
+            EventID: $('#hdEventID').val(),
+            Subject: $('#txtSubject').val().trim(),
+            Start: $('#txtStart').val().trim(),
+            End: $('#chkIsFullDay').is(':checked') ? null : $('#txtEnd').val().trim(),
+            Description: $('#txtDescription').val(),
+            ThemeColor: $('#ddThemeColor').val(),
+            IsFullDay: $('#chkIsFullDay').is(':checked')
+        }
+        SaveEvent(data);
+        // call function for submit data to the server
+    })
+    function SaveEvent(data) {
+        $.ajax({
+            type: "POST",
+            url: '/calendar/SaveEvent',
+            data: data,
+            success: function (data) {
+                if (data.status) {
+                    //Refresh the calendar
+                    FetchEventAndRenderCalendar();
+                    $('#myModalSave').modal('hide');
+                }
+            },
+            error: function () {
+                alert('Failed');
+            }
+        })
+    }
+})
---
+++ ProjectGraduationK/wwwroot/js/feed.js
@@ -0,0 +1,68 @@
+﻿$(document).ready(function () {
+
+    GetMessages();
+
+    $('#publishButton').click(function () {
+        PublishMessage();
+    });
+
+    function PublishMessage() {
+        let message = $('#inputMessage').val();
+        let author = $('#currentUserName').text();
+
+        $.ajax({
+            url: 'feed/SaveMessage',
+            type: 'POST',
+            data: {
+                "messageText": message,
+                "author": author
+            },
+            success: function () {
+                location.reload();
+            },
+            error: function () {
+                alert('Failed to publish a message');
+            }
+        })
+    }
+
+    function DeleteMessage(postId) {
+        $.ajax({
+            url: 'feed/'.concat(postId),
+            type: 'DELETE',
+            success: function () {
+                window.location.replace(window.location);
+            }
+        })
+    }
+
+    function GetMessages() {
+        $.ajax({
+            url: 'feed/GetMessages',
+            type: 'GET',
+            success: function (data) {
+                AppendMessagesToPage(data);
+                AppendDeletePostEvents();
+            }
+        })
+    }
+
+    function AppendMessagesToPage(messages) {
+        for (let i = Object.keys(messages).length - 1; i >= 0; i--) {
+            let date = new Date(messages[i].publishDate);
+            let formattedDate = date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes();
+
+            $('#messages').append('<div class="widget has-shadow"> <div class="widget-header d-flex align-items-center"> <div class="user-image"> <img class="rounded-circle" src="assets/img/avatar/avatar-04.jpg" alt="..."> </div><div class="d-flex flex-column mr-auto"> <div class="title"> <span class="username">' + messages[i].author + '</span> написал(а) </div><div class="time">' + formattedDate + '</div></div><div class="widget-options"> <div class="dropdown"> <button type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-toggle"> <i class="la la-ellipsis-h"></i> </button> <div class="dropdown-menu"> <a name="editPost" data-id="' + messages[i].messageId + '" class="dropdown-item"> <i class="la la-edit"></i>Редактировать</a> <a name="deletePost" href="javascript:void(0)" data-id="' + messages[i].messageId + '" class="dropdown-item"> <i class="la la-trash"></i>Удалить</a> </div></div></div></div><div class="widget-body"> <p>' + messages[i].messageText + '</p></div><div class="widget-footer d-flex align-items-center"> <div class="col-xl-8 col-md-8 col-7 no-padding d-flex justify-content-start align-items-center"> </div><div class="col-xl-4 col-md-4 col-5 no-padding d-flex justify-content-end"> <div class="meta"> <ul> <li> <a href="javascript:void(0);"> <i class="la la-comment"></i><span class="numb">12</span> </a> </li><li> <a href="javascript:void(0);"> <i class="la la-heart-o"></i><span class="numb">18</span> </a> </li></ul> </div></div></div></div>');
+        }
+    }
+
+    function AppendDeletePostEvents() {
+        let deletePostButtons = document.getElementsByName('deletePost');
+        for (let i = 0; i < deletePostButtons.length; i++) {
+            $(deletePostButtons[i]).click(function () {
+                let postId = $(deletePostButtons[i]).attr('data-id');
+                DeleteMessage(postId);
+            });
+        }
+    }
+})
