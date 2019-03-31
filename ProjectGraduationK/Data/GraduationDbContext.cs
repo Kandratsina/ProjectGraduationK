8f09356fe9b027e788ca7bd9badfac23bf0edb5a
Anastasia Kandratsina <kandratsina@gmail.com>
2019-03-31 22:50:45 +0300

    Add db context;

---
+++ ProjectGraduationK/Data/GraduationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProjectGraduationK.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectGraduationK.Data
+{
+    public class GraduationDbContext : IdentityDbContext<User>
+    {
+        public GraduationDbContext(DbContextOptions<GraduationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<FeedMessage> FeedMessages { get; set; }
+        public DbSet<CalendarEvent> CalendarEvents { get; set; }
+    }
+}
