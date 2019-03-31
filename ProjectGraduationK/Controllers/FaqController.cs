8897722a7cc5c6c50a6b2aa37826b24782c4124b
Anastasia Kandratsina <kandratsina@gmail.com>
2019-03-31 22:54:04 +0300

    Add faq;

---
+++ ProjectGraduationK/Controllers/FaqController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectGraduationK.Controllers
+{
+    [Route("faq")]
+    [Authorize]
+    public class FaqController : Controller
+    {
+        public IActionResult Faq()
+        {
+            return View();
+        }
+    }
+}
