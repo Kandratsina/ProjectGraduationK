a55ac1d41252d5b3e9f49891f99d154e3c0c4e45
Anastasia Kandratsina <kandratsina@gmail.com>
2019-04-02 01:10:06 +0300

    Add library;

---
+++ ProjectGraduationK/Controllers/LibraryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectGraduationK.Controllers
+{
+    [Route("library")]
+    [Authorize]
+    public class LibraryController : Controller
+    {
+        public IActionResult Library()
+        {
+            return View();
+        }
+    }
+}
